3d84bd482faef10dc311e2503f297ed558e394a0
xanqus <sbk98031816@gmail.com>
2022-08-01 15:52:30 +0900

    react todolist 1부

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [todo, setTodo] = useState("");
+  const onChange = (e) => {
+    setTodo(e.target.value);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>할 일</h1>
+      <form action="">
+        <input
+          type="text"
+          onChange={onChange}
+          value={todo}
+          placeholder="할 일을 적어주세요."
+        />
+      </form>
     </div>
   );
 }

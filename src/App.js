2deb8418ff150f2f6df1eb2fd7fa82acef0769ad
xanqus <sbk98031816@gmail.com>
2022-08-01 16:16:12 +0900

    할일 todos배열에 추가

--- src/App.js
+++ src/App.js
@@ -5,17 +5,38 @@ function App() {
   const onChange = (e) => {
     setTodo(e.target.value);
   };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (todo === "") {
+      alert("1글자 이상 입력해주세요");
+      return;
+    }
+    //////////
+
+    setTodos((currentArray) => [todo, ...currentArray]);
+    setTodo("");
+  };
   return (
     <div>
       <h1>할 일</h1>
-      <form action="">
+      <form onSubmit={onSubmit}>
         <input
           type="text"
           onChange={onChange}
           value={todo}
           placeholder="할 일을 적어주세요."
         />
+        <button>등록</button>
       </form>
+
+      <button
+        onClick={() => {
+          console.log(todos);
+        }}
+      >
+        check
+      </button>
     </div>
   );
 }

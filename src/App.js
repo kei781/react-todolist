e8ebdafe26ffb4be6a0a9b1450090abe76e0f15b
xanqus <sbk98031816@gmail.com>
2022-08-02 15:27:09 +0900

    todolist 리뉴얼 todo 추가

--- src/App.js
+++ src/App.js
@@ -1,60 +1,30 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
+import TodoInsert from "./components/TodoInsert";
+import TodoTemplate from "./components/TodoTemplate";
 function App() {
   const [todos, setTodos] = useState([]);
-  const [todo, setTodo] = useState("");
-  const onChange = (e) => {
-    setTodo(e.target.value);
+  const nextId = useRef(1);
+  const onInsert = (text) => {
+    const todo = {
+      id: nextId,
+      text: text,
+      checked: false,
+    };
+    setTodos((todos) => todos.concat(todo));
+    nextId.current++;
   };
 
-  const onSubmit = (e) => {
-    e.preventDefault();
-    if (todo === "") {
-      alert("1글자 이상 입력해주세요");
-      return;
-    }
-
-    // 새로운 배열을 만드는 방법
-    {
-      /*const newArr = [];
-    for (let i = 0; i < todos.length; i++) {
-      newArr[i] = todos[i];
-    }
-    newArr.push(todo);
-    setTodos(newArr);*/
-    }
-
-    // concat을 이용하는 방법
-    // setTodos((currentArray) => currentArray.concat(todo));
-    setTodos((currentArray) => [todo, ...currentArray]);
-    setTodo("");
-  };
   return (
-    <div>
-      <h1>할 일</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          type="text"
-          onChange={onChange}
-          value={todo}
-          placeholder="할 일을 적어주세요."
-        />
-        <button>등록</button>
-      </form>
-
+    <TodoTemplate>
+      <TodoInsert onInsert={onInsert} />
       <button
         onClick={() => {
           console.log(todos);
         }}
       >
-        check
+        show todos
       </button>
-      <hr />
-      <ul>
-        {todos.map((todo, index) => (
-          <li key={index}>{todo}</li>
-        ))}
-      </ul>
-    </div>
+    </TodoTemplate>
   );
 }
 
